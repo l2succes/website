862c2572ce45112ec316c8bcba04a91fdbbd1713
Luc Succes <l2succes@gmail.com>
2026-06-29 11:06:34 -0600

    Route Greenchip proposal page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/greenchip-proposal",
+        destination: "/greenchip-proposal/index.html",
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
